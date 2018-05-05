0430745a2c01c4ad6155c3f8f3723073e1097c69
Leon Bein <leon.bein@student.hpi.de>
2018-05-05 16:24:23 +0200

    Now workin trav lgith asfkjajdalf

--- TrueTurnTurnTurn/Blocks/Behaviors/GetTraversableBehavior.gd
+++ TrueTurnTurnTurn/Blocks/Behaviors/GetTraversableBehavior.gd
@@ -1,7 +1,7 @@
 extends "res://Blocks/Behaviors/BlockBehavior.gd"
 const Player = preload("res://Player.gd")
 
-
+var occluder
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
@@ -13,3 +13,5 @@ func setActive(isActive):
 	var mask = 2 if isActive else 1
 	block.set_collision_layer(mask)
 	block.set_collision_mask(mask)
+	mask = mask * mask * mask
+	block.get_node("Sprite").get_node("LightOccluder2D").set_occluder_light_mask(mask)
