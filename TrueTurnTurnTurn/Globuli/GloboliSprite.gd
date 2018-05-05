3021b0f4b7a726ec0fcbe6f896e07271e41646d4
Tom <brauntom2@t-online.de>
2018-05-05 13:33:06 +0200

    globuli color changes dependent on color set in globuli

---
+++ TrueTurnTurnTurn/Globuli/GloboliSprite.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const LightClass = preload("res://Light.gd")
+
+const whiteTexture = preload("res://Globuli/globuli_green.svg")
+const redTexture = preload("res://Globuli/globuli_red.svg")
+const greenTexture = preload("res://Globuli/globuli_green.svg")
+const orangeTexture = preload("res://Globuli/globuli_green.svg")
+
+const textureMap = {
+	LightClass.Colors.WHITE : whiteTexture,
+	LightClass.Colors.RED : redTexture,
+	LightClass.Colors.GREEN : greenTexture,
+	LightClass.Colors.ORANGE : orangeTexture
+}
+
+func getTexture(color):
+	return textureMap[color]
